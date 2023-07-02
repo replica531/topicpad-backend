ba313493ac8fa73d53801cf92c07aae8d6e0b05a
Sei Fukushima <sftakkyu1283@gmail.com>
2023-07-03 01:36:03 +0900

    feat: DB接続まで

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ up:
 		docker-compose up -d
 down:
 		docker-compose down --remove-orphans
+ps:
+		docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Networks}}\t{{.Mounts}}"
--- api/main.go
+++ api/main.go
@@ -1,13 +1,55 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "fmt"
+  "time"
+
+  "github.com/gin-gonic/gin"
+  "gorm.io/driver/mysql"
+  "gorm.io/gorm"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "wow! success!",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+  sqlConnect()
+
+  router := gin.Default()
+  router.LoadHTMLGlob("templates/*.html")
+
+  router.GET("/", func(ctx *gin.Context){
+    ctx.HTML(200, "index.html", gin.H{})
+  })
+
+  router.Run()
+}
+
+func sqlConnect() (database *gorm.DB) {
+  USER := "go_test"
+  PASSWORD := "password"
+  PROTOCOL := "tcp(mysql:3306)"
+  DBNAME := "go_database"
+
+  dsn := USER + ":" + PASSWORD + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	fmt.Println(dsn)
+  count := 0
+  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+  if err != nil {
+    for {
+      if err == nil {
+        fmt.Println("")
+        break
+      }
+      time.Sleep(time.Second)
+      count++
+      if count > 180 {
+        fmt.Println("")
+        fmt.Println("DB接続失敗")
+        panic(err)
+      }
+			fmt.Printf("retry(%d回目)\n", count)
+      db, err =  gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    }
+  }
+  fmt.Println("DB接続成功")
+
+  return db
 }

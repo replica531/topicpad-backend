6369405f09ea90912a754ef4ddeb962dae413dc5
Sei Fukushima <sftakkyu1283@gmail.com>
2023-07-07 04:10:03 +0900

    feat: frontendとの接続

--- Makefile
+++ Makefile
@@ -2,5 +2,7 @@ up:
 		docker-compose up -d
 down:
 		docker-compose down --remove-orphans
+build:
+		docker-compose build
 ps:
 		docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Networks}}\t{{.Mounts}}"
--- api/controller/user_controller.go
+++ api/controller/user_controller.go
@@ -11,14 +11,14 @@ type UserController struct{}
 
 // Index action: GET /users
 func (pc UserController) Index(c *gin.Context) {
-    var u repository.UserRepository
-    p, err := u.GetAll()
-    if err != nil {
-        c.AbortWithStatus(404)
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-    } else {
-        c.JSON(200, p)
-    }
+	var u repository.UserRepository
+	p, err := u.GetAll()
+	if err != nil {
+		c.AbortWithStatus(404)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(200, p)
+	}
 }
 
 
